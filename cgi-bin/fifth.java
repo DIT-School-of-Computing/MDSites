df2cddb4de34cd3375f3de4c20c906a0a38bd695
Mark Deegan <mark@dit.ie>
2015-04-25 16:09:18 +0100

    Changes to add comments to .java source files

--- cgi-bin/fifth.java
+++ cgi-bin/fifth.java
@@ -1,5 +1,11 @@
+/** Mark Deegan
+Sat 25 Apr 2015 16:05:06 IST
+*/
+
+/** class fifth for use in testing cgi-bin scripts on Apache server */
 public class fifth
 {
+	/** main method to print HTL code to send back to browser */
 	public static void main(String[] args)
 	{
 		System.out.println("Content-type:text/html");
@@ -11,8 +17,10 @@ public class fifth
 		System.out.println("</head>");
 		System.out.println("<body>");
 		System.out.println("<h2>Hello, World!<br>This is my first cgi-bin program in java (fifth.java).</h2>");
+
+		/** list out standard java environment properties from the server */
 		System.getProperties().list(System.out);
 		System.out.println("</body>");
 		System.out.println("</html>");
-	}
-}
+	} /** end main mthod */
+} /** end class declaration */
--- cgi-bin/sixth.java
+++ cgi-bin/sixth.java
@@ -1,7 +1,12 @@
+/** Mark Deegan
+Sat 25 Apr 2015 16:06:17 IST
+*/
+
 public class sixth
-{
+{ /** class sixth prints basic HTML when called as a cgi-bin script on Apache Server */
 	public static void main(String[] args)
-	{
+
+	{ /** declare main method to print some simple HTML */
 		System.out.println("Content-type:text/html");
 		System.out.println("");
 		System.out.println("");
@@ -13,5 +18,5 @@ public class sixth
 		System.out.println("<h2>Hello, World!<br>This is my second cgi-bin program in java (sixth.java)</h2>");
 		System.out.println("</body>");
 		System.out.println("</html>");
-	}
-}
+	} /** end declaration of main method */
+} /** end declaration of class sixth */
