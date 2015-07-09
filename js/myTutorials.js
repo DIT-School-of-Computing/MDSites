59d79af6f5cd34439875830cc5da3535b304545b
Mark Deegan <mark@dit.ie>
2015-07-09 21:19:19 +0100

    Adding various js and json related files

---
+++ js/myTutorials.js
@@ -0,0 +1,38 @@
+myFunction([
+{
+"display": "SCRIPT-Naoise: HTML Tutorial",
+"url": "http://www.w3schools.com/html/default.asp"
+},
+{
+"display": "SCRIPT-Seoid&#x00EDn: CSS Tutorial",
+"url": "http://www.w3schools.com/css/default.asp"
+},
+{
+"display": "SCRIPT-Odhr&#x00E1n: JavaScript Tutorial",
+"url": "http://www.w3schools.com/js/default.asp"
+},
+{
+"display": "SCRIPT-Hugh: jQuery Tutorial",
+"url": "http://www.w3schools.com/jquery/default.asp"
+},
+{
+"display": "SCRIPT-Marcus: JSON Tutorial",
+"url": "http://www.w3schools.com/json/default.asp"
+},
+{
+"display": "SCRIPT-Peter: AJAX Tutorial",
+"url": "http://www.w3schools.com/ajax/default.asp"
+},
+{
+"display": "SCRIPT-Kieran: SQL Tutorial",
+"url": "http://www.w3schools.com/sql/default.asp"
+},
+{
+"display": "SCRIPT-Michaela: PHP Tutorial",
+"url": "http://www.w3schools.com/php/default.asp"
+},
+{
+"display": "SCRIPT-Rockin' Horse: XML Tutorial",
+"url": "http://www.w3schools.com/xml/default.asp"
+}
+])
