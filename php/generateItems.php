9104b36286e66102d27f6f287060a92f1e03a768
Mark Deegan <mark@dit.ie>
2015-07-22 10:43:01 +0100

    Changes to reflect ability to run .html.en and .html as PHP scripts Other changes to provide parameters to PHP scripts for generation of list items and links

--- php/generateItems.php
+++ php/generateItems.php
@@ -1,18 +1,27 @@
-<?php 
+<?php
+// Mark Deegan
+// 21 July 2015 16:43:35 IST
+// PHP script to list the entries returned as facet categories
+// mock-up at this stage
+
+$argument1 = $_GET['arg1'];
+
 $x = 1;
-$max_lines = 5;
+$max_lines = $_GET['arg2'];;
+
+include './linkGeneration.php';
   
 // print the first list item
-echo "<li class=\"collapsed first\"><a href=\"~mark\" rel=\"nofollow\" class=\"facetapi-inactive\" id=\"facetapi-link--56\"> Java Programmes ($x)<span class=\"element-invisible\"> Apply Secondary general education filter ($x)</span></a></li>\n";
+echo "$firstPreamble$argument1 rel=\"nofollow\" class=\"facetapi-inactive\" id=\"facetapi-link--56\"> Java Programmes ($x)<span class=\"element-invisible\"> Apply Java Programmes ($x) filter</span></a></li>\n";
 $x++;
 
 // print all but the last list item
 while($x < $max_lines) {
-   echo "<li class=\"collapsed leaf\"><a href=\"~mark\" rel=\"nofollow\" class=\"facetapi-inactive\" id=\"facetapi-link--56\"> Java Programmes ($x)<span class=\"element-invisible\"> Apply Secondary general education filter ($x)</span></a></li>\n";
+   echo "$generalPreamble$argument1 rel=\"nofollow\" class=\"facetapi-inactive\" id=\"facetapi-link--56\"> Java Programmes ($x)<span class=\"element-invisible\"> Apply Java Programmes ($x) filter</span></a></li>\n";
    $x++;
 }
 
 // print the last list item
-echo "<li class=\"collapsed last\"><a href=\"~mark\" rel=\"nofollow\" class=\"facetapi-inactive\" id=\"facetapi-link--56\"> Java Programmes ($x)<span class=\"element-invisible\"> Apply Secondary general education filter ($x)</span></a></li>\n";
+echo "$lastPreamble$argument1 rel=\"nofollow\" class=\"facetapi-inactive\" id=\"facetapi-link--56\"> Java Programmes ($x)<span class=\"element-invisible\"> Apply Java Programmes ($x)</span></a></li>\n";
 
 ?>
---
+++ php/linkGeneration.php
@@ -0,0 +1,11 @@
+<?php
+// Mark Deegan
+// 22 July 2015 10:28:38 IST
+// PHP script to support the generation of list the entries returned as facet categories
+// mock-up at this stage
+
+$firstPreamble   = '<li class=\"collapsed first\"><a href=/';
+$generalPreamble = '<li class=\"collapsed leaf\"><a href=/';
+$lastPreamble    = '<li class=\"collapsed last\"><a href=/';
+
+?>
