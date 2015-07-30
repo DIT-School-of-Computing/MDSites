1ceca3a12b712befc6b84947002f3f9bbdc85d33
markdeegan <mark@dit.ie>
2015-07-30 11:30:17 +0100

    Changes to demoweb site from W3Schools

--- demoweb/script.js
+++ demoweb/script.js
@@ -1,5 +1,5 @@
 document.getElementById("foot01").innerHTML =
-"<p>&copy;  " + new Date().getFullYear() + " W3Schools. All rights reserved.</p>";
+"<p>&copy;  " + new Date().getFullYear() + " Mark Deegan for <a href=\"http://www.ncge.ie\"> NCGE.ie.</a> All rights reserved.</p>";
 
 document.getElementById("nav01").innerHTML =
 "<ul id='menu'>" +
