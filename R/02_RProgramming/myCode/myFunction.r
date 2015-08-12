412e19a02236b0ede494ede4e4a419d7fe573359
Mark Deegan <mark@dit.ie>
2015-08-12 17:49:28 +0100

    Changes to part1.R for Assignment 1

---
+++ R/02_RProgramming/myCode/myFunction.r
@@ -0,0 +1,10 @@
+myFunction <- function(x)
+{
+	y <- rnorm(100);
+	mean(y);
+}
+
+second <- function(x)
+{
+	x + rnorm(length(x))
+}
--- R/02_RProgramming/myNotes/part1.R
+++ R/02_RProgramming/myNotes/part1.R
@@ -1,13 +1,23 @@
 pollutantmean <- function(directory, pollutant, id = 1:332) {
     ## 'directory' is a character vector of length 1 indicating
     ## the location of the CSV files
+    print(directory);
     
     ## 'pollutant' is a character vector of length 1 indicating
     ## the name of the pollutant for which we will calculate the
     ## mean; either "sulfate" or "nitrate".
+    if((pollutant == "nitrate") || (pollutant == "sulfate"))
+    {
+      print(pollutant)
+    }
+    else
+    {
+      print("Not a valid pollutant name");
+    }
     
     ## 'id' is an integer vector indicating the monitor ID numbers
     ## to be used
+    print(length(id));
     
     ## Return the mean of the pollutant across all monitors list
     ## in the 'id' vector (ignoring NA values)
