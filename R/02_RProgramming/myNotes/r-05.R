50cd159de0efd343f23579509f1e6d1f62facb9c
markdeegan <mark@dit.ie>
2015-08-12 17:13:03 +0100

    No message.

---
+++ R/02_RProgramming/myNotes/r-05.R
@@ -0,0 +1,20 @@
+f <- function(a, b=1, c=2, d=NULL)
+{
+  
+}
+
+f2 <- function(a,b)
+{
+  a^2
+}
+
+f3 <- function(a, b)
+{
+  print(a);
+  print(b);
+}
+
+f4 <- function(x, y) 
+{
+  x^2 + y^2 / z
+}
