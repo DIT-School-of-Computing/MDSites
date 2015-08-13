f42cf47b3a344ffa3c8382a49f1bf307ce11249f
markdeegan <mark@dit.ie>
2015-08-13 16:02:46 +0100

    renamed part2.R to complete.R

--- R/02_RProgramming/myNotes/complete.R
+++ R/02_RProgramming/myNotes/complete.R
@@ -2,6 +2,7 @@ complete <- function(directory, id = 1:332) {
   ## 'directory' is a character vector of length 1 indicating
   ## the location of the CSV files
   
+  
   ## 'id' is an integer vector indicating the monitor ID numbers
   ## to be used
   
