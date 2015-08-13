b3c7fd5bc74d6bc81627955758b0b9ca6a8e3d82
markdeegan <mark@dit.ie>
2015-08-13 15:56:24 +0100

    Nearly completed exercise 2 in Assignment 1

--- R/02_RProgramming/myNotes/part2.R
+++ R/02_RProgramming/myNotes/part2.R
@@ -13,6 +13,9 @@ complete <- function(directory, id = 1:332) {
   ## where 'id' is the monitor ID number and 'nobs' is the
   ## number of complete cases
   
+  # this declares an empty data frame to be used to store the rsummary data
+  correctFrame = data.frame();
+  
   # add all data from the specified .csv files to the allData data frame
   # this code loops through all the values in the id argument to the function call
   # and for each, either creates (if it is the first value) or appends the records read from 
@@ -32,16 +35,38 @@ complete <- function(directory, id = 1:332) {
     # this checks if the allData frame already exists, 
     # if not, then we create it by reading in a .csv file
     if(!exists("allData")) {
+      # create a new vector, allData by reading the (first) .csv file specified
       allData <- read.csv(fname, header=TRUE);
+      
+      # # in the following statement, the complete.cases function elimintaes all NAs
+      # and the sum function counts all the TRUE entries in that vector
+      completeCount <- sum(complete.cases(allData));
+      
     } # end if statement
     # if it does exist, then we add the data from the data file to the excisting data frame using rbind
     else {
-      allData <- rbind(allData, read.csv(fname, header=TRUE));  
+      # we will (temporarily) store the new data in a vector of its own so we can count the 
+      # number of complete cases being read from the specified file
+      newData <- read.csv(fname, header=TRUE);
+      
+      # # in the following statement, the complete.cases function elimintaes all NAs
+      # and the sum function counts all the TRUE entries in that vector
+      completeCount <- sum(complete.cases(newData));
+      
+      # and now add the newData (including incomplete cases) to the allData vector
+      allData <- rbind(allData, newData);  
     } # end else statement
+    
+    # print out the number of complete cases for this iteration
+    print(c(as.numeric(i), as.numeric(completeCount)));
+    correctFrame <- rbind(correctFrame, c(as.numeric(i), as.numeric(completeCount)));
+    
   } # end for loop
   
-  print(nrow(allData));
-  ok <- complete.cases(allData);
-  sum(ok);
-
-}
+  # assign the correct column names to the data Frame
+  colnames(correctFrame) <- c("id","nobs")
+  
+  # print the resulting data frame
+  return(correctFrame);
+  
+} # end of for loop
