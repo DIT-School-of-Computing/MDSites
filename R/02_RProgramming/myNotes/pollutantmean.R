7dc2560e969434c3088f63119550cb68fad8a61c
markdeegan <mark@dit.ie>
2015-08-13 10:23:24 +0100

    CHanges for assignment 1

--- R/02_RProgramming/myNotes/pollutantmean.R
+++ R/02_RProgramming/myNotes/pollutantmean.R
@@ -22,13 +22,21 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
     ## Return the mean of the pollutant across all monitors list
     ## in the 'id' vector (ignoring NA values)
     ## NOTE: Do not round the result!
-    
-  
-    allData <- c();
-    
-    a <- read.csv("./specdata//001.csv", header=TRUE);
-    append(allData, a);
-
+        
+    # add all data from the specified .csv files to the allData data frame
+    for( i in 1:2) 
+    {
+      if(!exists("allData"))
+        {
+        print("in if");
+          allData <- read.csv("./specdata//001.csv", header=TRUE);
+        }
+      else
+      {
+        print("in else");
+        allData <- rbind(allData, read.csv("./specdata//002.csv", header=TRUE));  
+      }
+    }
     
     nrow(allData);
     
