02a713a32188ed41faf8a1a95bbf43a7b5ee629e
Mark Deegan <mark@dit.ie>
2015-08-28 18:09:48 +0100

    Working on Assignment-3

--- R/02_RProgramming/myNotes/Assignment-3/best.R
+++ R/02_RProgramming/myNotes/Assignment-3/best.R
@@ -1,30 +1,38 @@
+# function to load all the outcomes data
 getOutcomes <- function() {
   getOutcomes <- read.csv("outcome-of-care-measures.csv", colClasses="character");  
-}
+} # end of function getOutcomes
 
+# function to return a vector of valid state abbreviations
 getStatesVector <- function() {
   getStatesVector <- unique(getOutcomes()[,"State"])
-}
+} # end of function getStatesVector
 
+# get the data from the column 30DayMortalityRateHeartAttack
 get30DayMortalityRateHeartAttack <- function() {
   getOutcomesVector <- unique(getOutcomes()[1,11]);
-}
+} # enf of function get30DayMortalityRateHeartAttack
 
+# function to list the valid outcomes
 getValidOutcomes<- function() {
-  # outcomes <- getOutcomes()[0,c(11,17,23)];
+  # load the outcomes
   outcomes <- getOutcomes();
+  
+  # take columns 11, 17 and 23 for Heart Attack, Heart Failure and Pneumonia
+  # and strip the first 46 characters because the columns names are rather long
+  # and we only want the last part, heart.attack, heart.failure or pneumonia
   getValidOutcomes <- substring(names(outcomes)[c(11,17,23)], 46);
-}
+} # end of function getValidOutcomes
 
+# function to return the best hostpital in a given state for a given outcome
 best <- function(state, outcome) {
   # read outcome data
   outcomesData <- getOutcomes();
   
   # check the state and outcome are valid
-  # outcome <- gsub(' ','.', outcome);
-  print(outcome);
   
-  if(! is.element(state, getStatesVector())) {
+  # if the state name is not valid
+  if(! is.element(tolower(state), tolower(getStatesVector()))) {
     stop("invalid state");
   }
   
@@ -34,6 +42,7 @@ best <- function(state, outcome) {
   
   else {
     print("good state, good outcome");
+    outcomesData <- tolower(outcomesData$state) == tolower(state);
   }
   # return hospital name in that state with lowest 30-day death-rate
 }
